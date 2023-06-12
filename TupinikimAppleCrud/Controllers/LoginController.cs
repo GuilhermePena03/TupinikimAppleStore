21364bca59e6ee4dcb4b93e2801a440e4b767859
GuilhermePena03 <guilhermehenriquepena14@gmail.com>
2023-06-12 16:55:17 -0300

    Adicionar arquivos de projeto.

---
+++ TupinikimAppleCrud/Controllers/LoginController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Firebase.Auth;
+using Newtonsoft.Json;
+using static TupinikimAppleCrud.Models.LoginModel;
+using TupinikimAppleCrud.Models;
+
+namespace TupinikimAppleCrud.Controllers
+{
+    public class LoginController : Controller
+    {
+        FirebaseAuthProvider auth;
+        public LoginController()
+        {
+            auth = new FirebaseAuthProvider(
+                            new FirebaseConfig("AIzaSyBwDlWOAT8RhurOPW4MbzvrRC5yeGSkQ50"));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registration(LoginModel loginModel)
+        {
+            try
+            {
+                //create the user
+                await auth.CreateUserWithEmailAndPasswordAsync(loginModel.Email, loginModel.Password);
+                //log in the new user
+                var fbAuthLink = await auth
+                                .SignInWithEmailAndPasswordAsync(loginModel.Email, loginModel.Password);
+                string token = fbAuthLink.FirebaseToken;
+                //saving the token in a session variable
+                if (token != null)
+                {
+                    HttpContext.Session.SetString("_UserToken", token);
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (FirebaseAuthException ex)
+            {
+                //var firebaseEx = JsonConvert.DeserializeObject<FirebaseError>(ex.ResponseData);
+                //ModelState.AddModelError(String.Empty, firebaseEx.error.message);
+                //return View(loginModel);
+            }
+
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(LoginModel loginModel)
+        {
+            try
+            {
+                //log in an existing user
+                var fbAuthLink = await auth
+                                .SignInWithEmailAndPasswordAsync(loginModel.Email, loginModel.Password);
+                string token = fbAuthLink.FirebaseToken;
+                //save the token to a session variable
+                if (token != null)
+                {
+                    HttpContext.Session.SetString("_UserToken", token);
+
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (FirebaseAuthException ex)
+            {
+                //var firebaseEx = JsonConvert.DeserializeObject<FirebaseError>(ex.ResponseData);
+                //ModelState.AddModelError(String.Empty, firebaseEx.error.message);
+                //return View(loginModel);
+            }
+
+            return View("Index");
+        }
+
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+    }
+}
---
+++ TupinikimAppleCrud/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TupinikimAppleCrud.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        
+    }
+}
